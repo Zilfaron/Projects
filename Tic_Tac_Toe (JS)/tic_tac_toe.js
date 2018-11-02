d02dce15e46351d3e9ed69e50e764788b97a913c
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2018-11-02 19:20:22 +0500

    Added main variables

--- Tic_Tac_Toe (JS)/tic_tac_toe.js
+++ Tic_Tac_Toe (JS)/tic_tac_toe.js
@@ -1,5 +1,15 @@
-(function() {
+(function(d) {
 	"use strict";
+	console.log(!!d);
 
-	
-})();
+	let field 				 = [],
+			fieldSize 		 = 3,
+			currentPlayer  = null,
+			gameElem 			 = d.querySelector(".tic-tac-toe"),
+			gameStatusElem = d.querySelector(".game-status"),
+			rowClass 			 = "game-field__row",
+			cellClass 		 = "game-field__cell",
+			gameStarted 	 = false;	
+
+
+})(document);
