e754662cefa6b95eb0f7d353881e1e78bd956439
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2018-11-02 19:48:59 +0500

    createField function added

--- Tic_Tac_Toe (JS)/js/tic_tac_toe.js
+++ Tic_Tac_Toe (JS)/js/tic_tac_toe.js
@@ -11,5 +11,30 @@
 			cellClass 		 = "game-field__cell",
 			gameStarted 	 = false;	
 
+	function createField(container = d.body, size = 3, fieldClass = "field", rowClass = "row", cellClass = "cell") {
+		try {
+			if (size > 5 || size < 2) {
+				throw new Error("The size of the field is incorrect");
+			}
 
+			let fieldElem = createDOMElem("div", fieldClass);
+
+			for (let row = 0; row < size; row++) {
+				let rowElem = createDOMElem("div", rowClass);
+				field.push([]);
+
+				for (let cell = 0; cell < size; cell++) {
+					let cellElem = createDOMElem("div", cellClass, "free_cell");
+					rowElem.appendChild(cellElem);
+					field[row].push("");
+				}
+
+				fieldElem.appendChild(rowElem);
+			}
+
+			container.appendChild(fieldElem);
+		} catch(err) {
+			console.error(err.message);
+		}
+	}
 })(document);
