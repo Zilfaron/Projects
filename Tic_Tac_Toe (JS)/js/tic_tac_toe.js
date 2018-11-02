fb350dfda5fa2ecfac110955adb0b24911dc596a
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2018-11-02 22:10:12 +0500

    updateFieldMatrix function added

--- Tic_Tac_Toe (JS)/js/tic_tac_toe.js
+++ Tic_Tac_Toe (JS)/js/tic_tac_toe.js
@@ -1,7 +1,7 @@
 (function(d) {
 	"use strict";
 
-	let field 				 = [],
+	let fieldMatrix 	 = [],
 			fieldSize 		 = 3,
 			currentPlayer  = null,
 			gameElem 			 = d.querySelector(".tic-tac-toe"),
@@ -11,7 +11,7 @@
 			cellClass 		 = "game-field__cell",
 			gameStarted 	 = false;	
 
-	function createField(container = d.body, size = 3, fieldClass = "field", rowClass = "row", cellClass = "cell") {
+	function createField(fm, container = d.body, size = 3, fieldClass = "field", rowClass = "row", cellClass = "cell") {
 		try {
 			if (size > 5 || size < 2) {
 				throw new Error("The size of the field is incorrect");
@@ -21,12 +21,12 @@
 
 			for (let row = 0; row < size; row++) {
 				let rowElem = createDOMElem("div", rowClass);
-				field.push([]);
+				fm.push([]);
 
 				for (let cell = 0; cell < size; cell++) {
 					let cellElem = createDOMElem("div", cellClass, "free_cell");
 					rowElem.appendChild(cellElem);
-					field[row].push("");
+					fm[row].push("");
 				}
 
 				fieldElem.appendChild(rowElem);
@@ -37,7 +37,26 @@
 			console.error(err.message);
 		}
 	}
-	createField(gameElem, fieldSize, fieldClass, rowClass, cellClass);
-	let fieldElem = d.querySelector(fieldClass);
+	createField(fieldMatrix, gameElem, fieldSize, fieldClass, rowClass, cellClass);
+	let fieldElem = d.querySelector(`.${fieldClass}`);
+
+	function updateFieldMatrix(fm) {
+		let rowElems  = fieldElem.querySelectorAll(`.${rowClass}`);
+
+		for (let row = 0; row < rowElems.length; row++) {
+			let cellElems = rowElems[row].querySelectorAll(`.${cellClass}`),
+					cellsType = Array.prototype.map.call(cellElems, cell => cell.classList[1]);
+
+			for (let col = 0; col < cellElems.length; col++) {
+				if (cellsType[col] == "free_cell") {
+					fieldMatrix[row][col] = "";
+				} else if (cellsType[col] == "x_cell") {
+					fieldMatrix[row][col] = "X";
+				} else if (cellsType[col] == "o_cell") {
+					fieldMatrix[row][col] = "O";
+				}
+			}
+		}
+	}
 
 })(document);
